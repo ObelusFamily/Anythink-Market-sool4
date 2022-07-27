a9a4507fd49ba4abc6b3a9d19939b75bdca983d5
Brett Kinnamon <me@brettk.dev>
2022-07-27 19:03:25 -0400

    fix search title query

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -54,7 +54,7 @@ router.get("/", auth.optional, function(req, res, next) {
   }
 
   if (typeof req.query.title !== "undefined") {
-    query.title = { $text: { $search: req.query.title } }
+    query.title = { $regex: new RegExp(`.*${req.query.title}.*`) }
   }
 
   Promise.all([
