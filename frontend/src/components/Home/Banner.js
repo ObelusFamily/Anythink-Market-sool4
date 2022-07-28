fc5e8789e69042306bd6e69c567fb7df44370a50
Brett Kinnamon <me@brettk.dev>
2022-07-27 20:25:06 -0400

    add item searching to the frontend

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,22 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
 
-const Banner = () => {
+const Banner = (props) => {
+  const [query, setQuery] = React.useState('');
+
+  React.useEffect(() => {
+    if (query.length < 3) return;
+
+    fetch(`//localhost:3000/api/items?title=${query}`).then((r) => r.json()).then((data) => props.onItemsChange(data.items))
+  }, [query])
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
           <span id="get-part">A place to get</span>
+          <input type="search" id="search-box" value={query} onChange={(e) => setQuery(e.currentTarget.value)} />
           <span> the cool stuff.</span>
         </div>
       </div>
